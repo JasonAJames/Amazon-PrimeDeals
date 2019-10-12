2b4f56bc81b483957fa3ccd2948c95ede9476afe
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-10-12 09:35:41 -0700

    updated search

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ var router = express.Router();
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Amazon Prime Deals',
   CompanyName : "Amazon Prime Deals",
   HeaderLink1 : "Daily Deals",

4cb343fb2c489e891a9fea298e5e0b4ff739af96
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2021-01-03 13:16:32 -0800

    added Amazon Grocery page

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,27 @@ prodheading5 : "Amazon's 'Top Ranked' Daily Deals",
 prodheading6 : "Cryptocurrency Deals",
 prodheading7 : "Amazon Grocery Deals"  });
 });
+router.get('/grocery', function(req, res, next) {
+  res.render('grocery', { title: 'Amazon Grocery',
+  CompanyName : "Amazon Grocery Deals",
+  HeaderLink1 : "Daily Deals",
+  HeaderLink2 : "Coupons",
+  HeaderLink3 : "Give-Aways",
+  HeaderLink4 : "Business Deals",
+  welcome : "Welcome to",
+  pageheading : "Amazon Grocery Deals",
+  pagedescription1 : `Did you know Amazon is full of surprises?`,
+  pagedescription2 : `We'll show you the hidden, unknown, and undisclosed deals...`,
+  pagedescription3 : `So you can get the best out of your Amazon Shopping Experience`,
+  prodheading1 : "Shop",
+  prodheading2 : "Amazon",
+  prodheading3 : "Hidden Deals",
+  prodheading4 : "Try These Amazon Services",
+prodheading5 : "Amazon's 'Top Ranked' Daily Deals",
+prodheading6 : "Cryptocurrency Deals",
+prodheading7 : "Amazon Grocery Deals"  });
+ });
+});
 router.get('/coupons', function(req, res, next) {
   res.render('coupons', { title: 'Amazon Coupons',
   CompanyName : "Amazon Prime Deals",
